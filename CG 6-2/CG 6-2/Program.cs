6b93a441c1ca30762fb9fca3d9042d196465b3a8
Erin Hogan <hogan10@charter.net>
2018-08-16 23:48:01 -0500

    complete

---
+++ CG 6-2/CG 6-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace CG_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user to enter a sentence and hold on to the answer
+            Console.WriteLine("Please type a sentence: ");
+            string mySentence = Console.ReadLine();
+            //use the user's input and convert it to all caps and return that on screen
+            string upperSentence = mySentence.ToUpper();
+            Console.WriteLine(upperSentence);
+            Console.ReadLine();
+           
+        }
+        
+    }
+}
